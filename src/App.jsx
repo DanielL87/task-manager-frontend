b108f086c3cf6195bda7ed714e659df2fb874d27
Hara Hordofa <harahordofa@Haras-MacBook-Air.local>
2023-10-05 16:18:21 -0400

    updated main.jsx,created routes and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,69 @@
-import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [token, setToken] = useState("")
+  const [user, setUser] = useState({})
+  const [tasks, setTasks] = useState([])
+  const [categories, setCategories] = useState([])
+
+  async function fetchUser() {
+    const localToken = localStorage.getItem("token")
+
+    if (localToken) {
+      setToken(localToken)
+    }
+
+    if(!token) {
+      return;
+    }
+
+    const res = await fetch(`${API}/users/token`, {
+      headers:{
+        Authorization:`Bearer ${token}`
+      }
+    })
+    const info = await res.json();
+
+    if (info.success) {
+      setUser(info.user)
+    }
+  }
+
+  // fetch tasks 
+
+  async function fetchTasks() {
+    const res = await fetch(`${API}/tasks`)
+
+    const info = await res.json()
+
+    if(info.success){
+      setTasks(info.tasks)
+    }
+  }
+
+  // fetch categories 
+
+  async function fetchCategories() {
+    const res = await fetch(`${API}/categories`)
+
+    const info = await res.json()
+
+    if(info.success) {
+      setCategories(info.categories)
+    }
+  }
+
+  useEffect(() => {
+    fetchUser()
+  }, [token])
+
+  return(
+    <div>
+      <Outlet context={{setToken, token, user}}/>
+    </div>
+  );
 }
 
 export default App;
