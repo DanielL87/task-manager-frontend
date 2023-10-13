9978097b66c5efeb89fa3c00e2b35eee09345303
Vicpr22 <vicpr22@hotmail.com>
2023-10-13 18:02:30 -0400

    Tasks-Update

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import { FaPencilAlt } from "react-icons/fa";
 import { Link, useOutletContext } from "react-router-dom";
 
-
-
 export default function Tasks({ task }) {
   const { user } = useOutletContext();
+  const [completed, setCompleted] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setCompleted(!completed);
+  };
+
+  console.log(completed);
 
   return (
     <div className="" key={task.id}>
       <h4>{task.title}</h4>
+      <div>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={handleCheckboxChange}
+        />
+        <label>Completed</label>
+      </div>
       <p>{task.description}</p>
       <div>
         {user.id === task.userId && (
