5ce9c68a5815b8c630340e464ef9c775948b9003
Hara Hordofa <hhordofa1@gmail.com>
2023-10-13 16:30:34 -0400

    ..

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,43 +3,55 @@ import Category from "./Category";
 import Tasks from "./Tasks";
 
 export default function Home() {
-  const { categories, tasks } = useOutletContext();
+  const { categories, tasks, token, fetchTasks, fetchCategories, user } =
+    useOutletContext();
+  const { categoryName } = useParams();
+
+  const selectedTasks = tasks.filter(
+    (task) => task.category.name === categoryName
+  );
 
   return (
     <>
-      <div>
-        <div>
-          <Link to={"/createcategory"}>
-            <button>Create Category!</button>
-          </Link>
-          {categories.map((category) => {
-            return (
-              <Category
-                key={category.id}
-                category={category}
-                token={token}
-                fetchCategories={fetchCategories}
-                fetchTasks={fetchTasks}
-              />
-            );
-          })}
-        </div>
-        <div>
-
-          <div >
-            {tasks.map((task) => {
+      {user.id ? (
+        <div id="body-container">
+          <div>
+            <Link to={"/createcategory"}>
+              <button>Create Category!</button>
+            </Link>
+            {categories.map((category) => {
               return (
-                <div className="task" key={task.id} >
-                  <Tasks task={task} />
-                </div>
+                <Category
+                  key={category.id}
+                  category={category}
+                  token={token}
+                  fetchCategories={fetchCategories}
+                  fetchTasks={fetchTasks}
+                />
               );
             })}
           </div>
-        </div>
-        <div>
-          <Link to={"/createTask"}>
-            <button>+ New Task</button>
-          </Link>
+          <div>
+            <div>
+              {" "}
+              <div>
+                {selectedTasks.map((task) => {
+                  return (
+                    <div className="task" key={task.id}>
+                      <Tasks task={task} />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div>
+              {categoryName && (
+                <Link to={"/createTask"}>
+                  <button>+ New Task</button>
+                </Link>
+              )}
+            </div>
+          </div>
         </div>
       ) : (
         <div>Please Login</div>
